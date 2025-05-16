0a43dcb730d1a9d35c27844496fd4b963fda9fa1
Fabio-Bianco <biancobfabio@gmail.com>
2025-05-16 02:47:11 +0200

    #

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,3 +1,4 @@
+// src/components/MovieCard.jsx
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar as solidStar } from "@fortawesome/free-solid-svg-icons";
@@ -7,7 +8,7 @@ import "./MovieCard.css";
 export default function MovieCard({ data }) {
   const navigate = useNavigate();
   const {
-    slug,
+    id,
     title,
     director,
     genre,
@@ -25,7 +26,7 @@ export default function MovieCard({ data }) {
   const fullStars = Math.round(vote);
 
   const handleClick = () => {
-    navigate(`/movies/${slug}`); // ✅ usa slug
+    navigate(`/movies/${id}`);
   };
 
   return (
@@ -42,8 +43,8 @@ export default function MovieCard({ data }) {
         />
 
         <div className="card-body d-flex flex-column">
-          <h5 className="card-title" style={{ color: "#fff" }}>{title}</h5>
-          <p className="card-subtitle mb-2" style={{ color: "#ffcc00" }}>
+          <h5 className="card-title">{title}</h5>
+          <p className="card-subtitle mb-2">
             🎬 {director} | {genre || "N/D"} | {release_year || "N/D"}
           </p>
 
